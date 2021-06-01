a1aa8cc0d0a7ff83f6da2722c49b5141a6a320c5
yurik <yurik.valder@gmail.com>
2021-06-01 05:28:28 -0400

    updated app!

--- index.js
+++ index.js
@@ -14,16 +14,35 @@ app.listen(3000, () => {
 app.use(express.static('public'))
 
 
-async function callWeather(){
+// async function callWeather(){
+//     const apiKey = process.env.API_KEY; 
+//     const zipCodes = [29301, 29401]; 
+//     const urlApi = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCodes[1]},us&appid=${apiKey}`; 
+//     const responce = await fetch(urlApi); 
+//     const formatedData = await responce.json(); 
+
+//     console.log(`city: ${formatedData.name}`); 
+//     console.log(`Ight my g, it humid af: ${formatedData.main.humidity}`); 
+//     //mydb.insert(formatedData); 
+// }
+
+//creating new endpoint for a get requeset
+//that will recieve the data from the client 
+app.get('/weather/:latlong', async (req, res) => {
+    //using the request object in the call back to 
+    //retrieve the route parameters passed in from the client 
+    const latlong = req.params.latlong.split(','); 
+    const lat = latlong[0]; 
+    const lon = latlong[1]; 
     const apiKey = process.env.API_KEY; 
-    const zipCodes = [29301, 29401]; 
-    const urlApi = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCodes[1]},us&appid=${apiKey}`; 
-    const responce = await fetch(urlApi); 
-    const formatedData = await responce.json(); 
 
-    console.log(`city: ${formatedData.name}`); 
-    console.log(`Ight my g, it humid af: ${formatedData.main.humidity}`); 
-    //mydb.insert(formatedData); 
-}
+    const urlAPi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`; 
+    const apiData = await fetch(urlAPi); 
+    const formatData = await apiData.json(); 
 
-callWeather();
+    console.log(formatData); 
+    //responce to the client 
+    res.json(formatData); 
+})
+
+//callWeather();
